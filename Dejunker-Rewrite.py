caaba4a42d0505a82a57a24bbfa9d994b45d2968
Kipland Melton <kipland@kiplands-mbp.home>
2021-02-02 09:56:44 -0600

    correctly filling directoryPaths, sortable and unsortable not working

--- Dejunker-Rewrite.py
+++ Dejunker-Rewrite.py
@@ -6,25 +6,26 @@ def main():
 	sortableFiles = []
 	# Will contain all directories of folders that are within the directory to be sorted
 	unsortableFiles = []
+	# Will be populated with filepaths of all the items inside a given directory
+	directoryPaths = []
 
 	chosenDirectory = raw_input("Enter directory: ")
+
 
 	if os.path.isdir(chosenDirectory) == True:
 		directoryContents = os.listdir(chosenDirectory)
 
-		counter = 0
+	for x in range(len(directoryContents)):
+		directoryPaths.append(os.path.join(chosenDirectory, directoryContents[x]))
 
-		for entry in directoryContents:
-			if os.path.isdir(entry) == True:
-				unsortableFiles.append(directoryContents[counter])
-			
-			else:
-				sortableFiles.append(directoryContents[counter])
-			
-			counter += 1
 
-	print(sortableFiles)
-	print(unsortableFiles)
+
+	print("Sortable")
+	for entry in sortableFiles:
+		print(entry)
+	print("Unsortable")
+	for entry in unsortableFiles:
+		print(entry)
 
 if __name__ == '__main__':
 	main()
